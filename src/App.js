e85954aaef7d5c1ae3a690d6d90306fd1f7bcf34
Mike Bond <mikebond84@gmail.com>
2018-09-06 13:05:56 -0400

    additions in class

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ class App extends React.Component {
       typingScreen: '',
       answerScreen: ''
     }
-    this.handleClick = this.handleClick.bind(this)
   }
   // defined constructor, called super method, line 8&9 set state
   // explicitly set value of 'this'
@@ -18,53 +17,54 @@ class App extends React.Component {
       <table id='frame'>
         <thead id='display'>
           <tr>
-            <th name='typingScreen' />
-            <th name='answerScreen' />
+            <th name='typingScreen'>{this.state.typingScreen}</th>
+            <th name='answerScreen'>{this.state.answerScreen}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td id='Clear'><button onClick={(event) => this.handleClick()} data-action='clear'>C</button></td>
-            <td id='Decimal'><button onClick={(event) => this.handleClick()} data-action='decimal'>.</button></td>
-            <td id='Calculate'><button onClick={(event) => this.handleClick()} data-action='calculate'>=</button></td>
+            <td id='Clear'><button onClick={(event) => this.handleClick(event)} data-action='clear'>C</button></td>
+            <td id='Decimal'><button onClick={(event) => this.handleClick(event)} data-action='decimal'>.</button></td>
+            <td id='Calculate'><button onClick={(event) => this.handleClick(event)} data-action='calculate'>=</button></td>
           </tr>
           <tr>
-            <td id='7'><button onClick={(event) => this.handleClick()}>7</button></td>
-            <td id='8'><button onClick={(event) => this.handleClick()}>8</button></td>
-            <td id='9'><button onClick={(event) => this.handleClick()}>9</button></td>
+            <td id='7'><button onClick={(event) => this.handleClick(event)}>7</button></td>
+            <td id='8'><button onClick={(event) => this.handleClick(event)}>8</button></td>
+            <td id='9'><button onClick={(event) => this.handleClick(event)}>9</button></td>
           </tr>
           <tr>
-            <td id='4'><button onClick={(event) => this.handleClick()}>4</button></td>
-            <td id='5'><button onClick={(event) => this.handleClick()}>5</button></td>
-            <td id='6'><button onClick={(event) => this.handleClick()}>6</button></td>
+            <td id='4'><button onClick={(event) => this.handleClick(event)}>4</button></td>
+            <td id='5'><button onClick={(event) => this.handleClick(event)}>5</button></td>
+            <td id='6'><button onClick={(event) => this.handleClick(event)}>6</button></td>
           </tr>
           <tr>
-            <td id='1'><button onClick={(event) => this.handleClick()}>1</button></td>
-            <td id='2'><button onClick={(event) => this.handleClick()}>2</button></td>
-            <td id='3'><button onClick={(event) => this.handleClick()}>3</button></td>
+            <td id='1'><button onClick={(event) => this.handleClick(event)}>1</button></td>
+            <td id='2'><button onClick={(event) => this.handleClick(event)}>2</button></td>
+            <td id='3'><button onClick={(event) => this.handleClick(event)}>3</button></td>
           </tr>
           <tr>
-            <td id='0'><button onClick={(event) => this.handleClick()}>0</button></td>
-            <td id='Add'><button onClick={(event) => this.handleClick()} data-action='add'>+</button></td>
-            <td id='Subtract'><button onClick={(event) => this.handleClick()} data-action='subtract'>-</button></td>
+            <td id='0'><button onClick={(event) => this.handleClick(event)}>0</button></td>
+            <td id='Add'><button onClick={(event) => this.handleClick(event)} data-action='add'>+</button></td>
+            <td id='Subtract'><button onClick={(event) => this.handleClick(event)} data-action='subtract'>-</button></td>
           </tr>
           <tr>
-            <td id='Multiply'><button onClick={(event) => this.handleClick()} data-action='multiply'>*</button></td>
-            <td id='Divide'><button onClick={(event) => this.handleClick()} data-action='divide'>÷</button></td>
-            <td id='Calculate'><button onClick={(event) => this.handleClick()} data-action='calculate'>=</button></td>
+            <td id='Multiply'><button onClick={(event) => this.handleClick(event)} data-action='multiply'>*</button></td>
+            <td id='Divide'><button onClick={(event) => this.handleClick(event)} data-action='divide'>÷</button></td>
+            <td id='Calculate'><button onClick={(event) => this.handleClick(event)} data-action='calculate'>=</button></td>
           </tr>
         </tbody>
       </table>
     )
   }
   handleClick (event) {
-    const value = event.target.value
-    if ('calculate') {
+    const action = event.target.dataset.action
+    if (action === 'calculate') {
       const answerScreen = eval(this.state.typingScreen).toString()
-      this.setState({answerScreen})
-    } else {
-      this.button = ('clear')
+      this.setState({answerScreen: answerScreen})
+    } else if (action === 'clear') {
       this.setState({typingScreen: ''})
+    } else {
+      this.setState({typingScreen: this.state.typingScreen + event.target.innerText})
     }
   }
 }
